4ecd34522be09247a88e19783bb7efb56a1246ad
Levangul <150108077+Levangul@users.noreply.github.com>
2024-03-15 02:13:51 -0400

    final code added readme

--- server.js
+++ server.js
@@ -27,33 +27,32 @@ startQuestions = () => {
       "Add a department",
       "Add a role",
       "Add an employee",
-      "Update an employee role",
-      "Exit"
+      "Update an employee role"
     ],
   }])
-  .then((response) => {
-    const choice = response.initial_questions;
+    .then((response) => {
+      const choice = response.initial_questions;
 
-    if (choice === "View all departments") {
-      viewDepartments();
-    } else if (choice === "View all roles") {
-      viewRoles();
-    } else if (choice === "View all employees") {
-      viewEmployees();
-    } else if (choice === "Add a department") {
-      addDepartment();
-    } else if (choice === "Add a role") {
-      addRole();
-    } else if (choice === "Add an employee") {
-      addEmployee();
-    } else if (choice === "Update an employee role") {
-      process.exit();
-    }
-  });
+      if (choice === "View all departments") {
+        viewDepartments();
+      } else if (choice === "View all roles") {
+        viewRoles();
+      } else if (choice === "View all employees") {
+        viewEmployees();
+      } else if (choice === "Add a department") {
+        addDepartment();
+      } else if (choice === "Add a role") {
+        addRole();
+      } else if (choice === "Add an employee") {
+        addEmployee();
+      } else if (choice === "Update an employee role") {
+        updateEmployeeRole();
+      }
+    });
 };
 
 // VEIW DEPARTMENTS FUNCTION
-function viewDepartments () {
+function viewDepartments() {
   db.query('SELECT department.id AS Department_ID, department.name AS Department_Name FROM department', (err, res) => {
     if (err) {
       throw err;
@@ -65,7 +64,7 @@ function viewDepartments () {
 }
 
 // VIEW ROLES FUNCTION
-function viewRoles () {
+function viewRoles() {
   db.query('SELECT role.id AS Role_ID, role.title AS Role_Title, role.salary AS Role_Salary, department.name AS Role_Department_ID FROM role INNER JOIN department ON role.department_id = department.id;', (err, res) => {
     if (err) {
       throw err;
@@ -77,7 +76,7 @@ function viewRoles () {
 }
 
 // VIEW EMPLOYEES FUNCTION
-function viewEmployees () {
+function viewEmployees() {
   db.query('SELECT employee.id AS Employee_ID, CONCAT(employee.first_name, " ", employee.last_name) AS Employee_Name, role.title AS Employee_Role, role.salary AS Employee_Salary, CONCAT(e2.first_name, " ", e2.last_name) AS Employee_Manager FROM employee INNER JOIN role ON employee.role_id = role.id LEFT JOIN employee as e2 ON e2.id = employee.manager_id', (err, res) => {
     if (err) {
       throw err;
@@ -97,15 +96,15 @@ function addDepartment() {
       name: "new_department"
     }
   ])
-  .then((input) => {
-    db.query('INSERT INTO department SET ?', { name: input.new_department }, (err, res) => {
-      if (err) {
-        throw err;
-      }
-      console.log("New department added successfully!");
-      startQuestions();
+    .then((input) => {
+      db.query('INSERT INTO department SET ?', { name: input.new_department }, (err, res) => {
+        if (err) {
+          throw err;
+        }
+        console.log("New department added successfully!");
+        startQuestions();
+      });
     });
-  });
 }
 
 // ADD ROLE FUNCTION
@@ -128,16 +127,16 @@ function addRole() {
       name: "departmentId"
     }
   ])
-  .then((input) => {
-    db.query('INSERT INTO role SET ?', {
-      title: input.title,
-      salary: input.salary,
-      department_id: input.departmentId
-    }, (err, res) => {
-      if (err) throw err;
-      startQuestions();
+    .then((input) => {
+      db.query('INSERT INTO role SET ?', {
+        title: input.title,
+        salary: input.salary,
+        department_id: input.departmentId
+      }, (err, res) => {
+        if (err) throw err;
+        startQuestions();
+      });
     });
-  });
 }
 
 // ADD EMPLOYEE FUNCTION
@@ -164,18 +163,39 @@ function addEmployee() {
       name: "managerId"
     }
   ])
-  .then((input) => {
-    db.query('INSERT INTO employee SET ?', {
-      first_name: input.firstName,
-      last_name: input.lastName,
-      role_id: input.roleId,
-      manager_id: input.managerId
-    }, (err, res) => {
-      if (err) throw err;
-      console.log("employee added successfully!");
-      startQuestions();
+    .then((input) => {
+      db.query('INSERT INTO employee SET ?', {
+        first_name: input.firstName,
+        last_name: input.lastName,
+        role_id: input.roleId,
+        manager_id: input.managerId
+      }, (err, res) => {
+        if (err) throw err;
+        console.log("employee added successfully!");
+        startQuestions();
+      });
     });
-  });
+}
+function updateEmployeeRole() {
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "Enter the ID of the employee you want to update:",
+      name: "employeeId"
+    },
+    {
+      type: "input",
+      message: "Enter the new role ID for the employee:",
+      name: "newRoleId"
+    }
+  ])
+    .then((input) => {
+      db.query('UPDATE employee SET role_id = ? WHERE id = ?', [input.newRoleId, input.employeeId], (err, res) => {
+        if (err) throw err;
+        console.log("Employee role updated successfully!");
+        startQuestions();
+      });
+    });
 }
 startQuestions();
 
